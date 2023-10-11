201be394d0110a141d8398017c456108db3c2cf9
AndrewTG5 <andy123llk@gmail.com>
2023-10-12 07:52:15 +1300

    lock orientation to portrait, bug fixes and update readme

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This is a Minesweeper game written in Kotlin using Jetpack Compose.
 
+<img src="Screenshot1.png" height="500" /> <img src="Screenshot2.png" height="500" />
+
+
 Some cool features:
 - Material3 Dynamic Color
 - Predictive back gesture
@@ -10,3 +13,7 @@ TODO:
 - Make the game configurable
 - Add a timer
 - Generate the board based on the first click
+- Allow non portrait orientation
+- Improve performance
+- Make it so all tiles must be dug in order to win
+- Custom app icon
--- app/src/main/java/com/andrewblake/minesweeper/MainActivity.kt
+++ app/src/main/java/com/andrewblake/minesweeper/MainActivity.kt
@@ -37,20 +37,20 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import com.andrewblake.minesweeper.ui.theme.MinesweeperTheme
 
-private const val height: Int = 20
-private const val width: Int = 10
-private const val mines: Int = 35
+private const val HEIGHT: Int = 20
+private const val WIDTH: Int = 10
+private const val MINES: Int = 35
 
 private var tileIsMine: Array<Array<Boolean>> =
-    Array(height) {
-        Array(width) {
+    Array(HEIGHT) {
+        Array(WIDTH) {
             false
         }
     }
 
 private var tileNearbyMines: Array<Array<Int>> =
-    Array(height) {
-        Array(width) {
+    Array(HEIGHT) {
+        Array(WIDTH) {
             0
         }
     }
@@ -62,33 +62,38 @@ class MainActivity : ComponentActivity() {
 
         // reset tileIsMine and tileNearbyMines, for when the user presses the refresh button
         tileIsMine =
-            Array(height) {
-                Array(width) {
+            Array(HEIGHT) {
+                Array(WIDTH) {
                     false
                 }
             }
         tileNearbyMines =
-            Array(height) {
-                Array(width) {
+            Array(HEIGHT) {
+                Array(WIDTH) {
                     0
                 }
             }
 
-        // populate tiles with mine and number blocks
-        var mineCount = mines
-        for (i in 0 until mineCount) {
-            // randomly place mines
-            val x = (0 until width).random()
-            val y = (0 until height).random()
-            // if the tile is not already a mine, make it a mine
+        // populate tiles with mines and number blocks
+        var mineCount = MINES
+        while (mineCount > 0) {
+            val x = (0 until WIDTH).random()
+            val y = (0 until HEIGHT).random()
             if (!tileIsMine[y][x]) {
                 tileIsMine[y][x] = true
                 mineCount--
-                // increment nearby tiles
-                for (k in -1..1) {
-                    for (l in -1..1) {
-                        if (y + k in 0 until height && x + l in 0 until width) {
-                            tileNearbyMines[y + k][x + l]++
+            }
+        }
+        for (i in 0 until HEIGHT) {
+            for (j in 0 until WIDTH) {
+                if (!tileIsMine[i][j]) {
+                    for (k in -1..1) {
+                        for (l in -1..1) {
+                            if (i + k in 0 until HEIGHT && j + l in 0 until WIDTH) {
+                                if (tileIsMine[i + k][j + l]) {
+                                    tileNearbyMines[i][j]++
+                                }
+                            }
                         }
                     }
                 }
@@ -137,7 +142,7 @@ class MainActivity : ComponentActivity() {
                                     }
                                 },
                                 actions = {
-                                    val remainingMines = mines - flagsPlaced // display remaining mines
+                                    val remainingMines = MINES - flagsPlaced // display remaining mines
                                     Text("🚩 $remainingMines")
                                 },
                                 scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
@@ -152,16 +157,16 @@ class MainActivity : ComponentActivity() {
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
                             var tileIsDug = remember {
-                                Array(height) {
-                                    Array(width) {
+                                Array(HEIGHT) {
+                                    Array(WIDTH) {
                                         mutableStateOf(false)
                                     }
                                 }
                             }
 
                             val tileIsFlagged = remember {
-                                Array(height) {
-                                    Array(width) {
+                                Array(HEIGHT) {
+                                    Array(WIDTH) {
                                         mutableStateOf(false)
                                     }
                                 }
@@ -209,9 +214,9 @@ class MainActivity : ComponentActivity() {
                             }
 
                             Column {
-                                for (i in 0 until height) { // create tiles
+                                for (i in 0 until HEIGHT) { // create tiles
                                     Row {
-                                        for (j in 0 until width) {
+                                        for (j in 0 until WIDTH) {
                                             Tile(
                                                 x = j,
                                                 y = i,
@@ -239,11 +244,13 @@ class MainActivity : ComponentActivity() {
                                                         tileIsDug = digTiles(j, i, tileIsDug)
                                                     }
                                                 } else {
-                                                    tileIsFlagged[i][j].value = !tileIsFlagged[i][j].value
-                                                    if (tileIsFlagged[i][j].value) {
-                                                        flagsPlaced++
-                                                    } else {
-                                                        flagsPlaced--
+                                                    if (!tileIsDug[i][j].value) {
+                                                        tileIsFlagged[i][j].value = !tileIsFlagged[i][j].value
+                                                        if (tileIsFlagged[i][j].value) {
+                                                            flagsPlaced++
+                                                        } else {
+                                                            flagsPlaced--
+                                                        }
                                                     }
                                                 }
                                                 if (checkLost(tileIsDug)) { // check if lost or won
@@ -312,8 +319,8 @@ fun digTiles(
 
         if (tileIsMine[y][x]) {
             // dig every mine
-            for (i in 0 until height) {
-                for (j in 0 until width) {
+            for (i in 0 until HEIGHT) {
+                for (j in 0 until WIDTH) {
                     if (tileIsMine[i][j]) {
                         newtileIsDug[i][j].value = true
                     }
@@ -322,7 +329,7 @@ fun digTiles(
         } else if (tileNearbyMines[y][x] == 0 && !tileIsMine[y][x]) {
             for (i in -1..1) {
                 for (j in -1..1) {
-                    if (y + i in 0 until height && x + j in 0 until width) {
+                    if (y + i in 0 until HEIGHT && x + j in 0 until WIDTH) {
                         newtileIsDug =
                             digTiles(x + j, y + i, newtileIsDug) // recursively dig nearby tiles
                     }
@@ -338,8 +345,8 @@ fun digTiles(
  * @return Whether the user has lost or not (dug a mine)
  */
 fun checkLost(tileIsDug: Array<Array<MutableState<Boolean>>>): Boolean {
-    for (i in 0 until height) {
-        for (j in 0 until width) {
+    for (i in 0 until HEIGHT) {
+        for (j in 0 until WIDTH) {
             if (tileIsDug[i][j].value && tileIsMine[i][j]) {
                 return true
             }
@@ -353,8 +360,8 @@ fun checkLost(tileIsDug: Array<Array<MutableState<Boolean>>>): Boolean {
  * @return Whether the user has won or not (flagged all mines). If the user has flagged a tile that is not a mine, they have not won.
  */
 fun checkWin(tileIsFlagged: Array<Array<MutableState<Boolean>>>): Boolean {
-    for (i in 0 until height) {
-        for (j in 0 until width) {
+    for (i in 0 until HEIGHT) {
+        for (j in 0 until WIDTH) {
             if (tileIsMine[i][j] && !tileIsFlagged[i][j].value || !tileIsMine[i][j] && tileIsFlagged[i][j].value) {
                 return false
             }
